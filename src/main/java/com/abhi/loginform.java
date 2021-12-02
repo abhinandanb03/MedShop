761700225c42a358b160f48c7a2c829528dad59e
abhinandanb03 <abhinandanb03@gmail.com>
2021-12-02 22:52:11 +0530

    added servlet for login

---
+++ src/main/java/com/abhi/loginform.java
@@ -0,0 +1,33 @@
+package com.abhi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class loginform extends HttpServlet {
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String email = request.getParameter("email");
+        String pass = request.getParameter("password");
+        if(Validate.checkUser(email, pass))
+        {
+            RequestDispatcher rs = request.getRequestDispatcher("Welcome");
+            rs.forward(request, response);
+        }
+        else
+        {
+           out.println("Username or Password incorrect");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+        }
+    }  
+}
---
+++ src/main/java/com/abhi/signup.java
@@ -0,0 +1,57 @@
+package com.abhi;
+
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+
+
+public class signup extends HttpServlet {
+   
+
+   @Override
+   public void doGet(HttpServletRequest req, HttpServletResponse res) 
+         throws ServletException, IOException {
+
+      PrintWriter out = null;
+
+      res.setContentType("text/html");
+      out = res.getWriter();
+      
+      PreparedStatement pst;
+      Connection con = null;
+      try {
+      Class.forName("com.mysql.jdbc.Driver").newInstance();
+      con = DriverManager.getConnection("jdbc:mysql://localhost:3306/medshop", "root", "");
+      String email = req.getParameter("email");
+      String pass = req.getParameter("password");
+      pst = con.prepareStatement("insert into userdetails(email,password)values(?,?)");
+      pst.setString(1, email);
+      pst.setString(2, pass);
+      pst.executeUpdate();  
+      out.println("inserted");
+      pst.executeUpdate();  
+      out.println("Database test created sucessfully.");
+      }
+      catch (Exception e)
+      {
+      out.println("An error occurred."+e);
+      }
+      }
+   
+
+   @Override
+   public void doPost(HttpServletRequest req, HttpServletResponse res) 
+         throws ServletException, IOException {
+      doGet(req, res);
+   }
+}
